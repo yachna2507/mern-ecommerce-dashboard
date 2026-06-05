406f90b237dafe1fb764fe5c19e5371bcb9da0c1
yachna2507 <Singh.yachna25@gmail.com>
2026-06-05 18:34:52 +0530

    Update axios.js

--- dashboard/src/api/axios.js
+++ dashboard/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "https://mern-ecommerce-dashboard-1.onrender.com"
 });
 
 api.interceptors.request.use(
