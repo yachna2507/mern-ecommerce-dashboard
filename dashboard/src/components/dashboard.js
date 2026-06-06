8b3cf12d3a37fee6ccaf5fa5d4481db440ba212d
yachna2507 <Singh.yachna25@gmail.com>
2026-06-06 10:17:37 +0530

    Update dashboard.js

--- dashboard/src/components/dashboard.js
+++ dashboard/src/components/dashboard.js
@@ -1,7 +1,7 @@
 import React , { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {jwtDecode } from "jwt-decode";
-import axios from "../api/axios";
+import api from "../api/axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
