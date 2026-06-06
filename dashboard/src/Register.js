323cc6cea85dec7a7ed11db18a5b2523cb6942d2
yachna2507 <Singh.yachna25@gmail.com>
2026-06-06 12:39:34 +0530

    Update Register.js

--- dashboard/src/Register.js
+++ dashboard/src/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../api/axios";
+import api from "./api/axios";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
